48179961bd6eb2b45a482744533549da6b80cf93
James Meyer <Jamesmeyer75@gmail>
2020-02-04 17:02:41 -0600

    added to the sign-in

---
+++ config/config.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'dev',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    define: {
+      charset: 'utf8',
+      timestamps: true,
+      underscored: true,
+    },
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    define: {
+      charset: 'utf8',
+      timestamps: true,
+      underscored: true,
+    },
+  },
+  production: {
+    use_env_variable: 'JAWSDB_URL',
+    dialect: 'mysql',
+    define: {
+      charset: 'utf8',
+      timestamps: true,
+      underscored: true,
+    },
+    logging: false,
+  },
+};
