2a7404c35d7cb15c86dfab0b798d1882c41b30b2
ggeorge <gmgeorge1993@gmail.com>
2020-02-04 18:57:47 -0600

    added nav

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from '../logo.svg';
+import Nav from '../components/nav/index.js';
 import '../App.css';
 
 function Home() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-        </p>
-                <p>Hello World</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-        </a>
-            </header>
-        </div>
+        <Nav />
     );
 }
 
