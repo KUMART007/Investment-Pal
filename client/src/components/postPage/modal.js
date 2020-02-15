9335b163289d6441ad44a4c78ede6356ef02eb72
ggeorge <gmgeorge1993@gmail.com>
2020-02-15 10:59:51 -0600

    added layout to posts page

---
+++ client/src/components/postPage/modal.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input } from 'reactstrap';
+
+const PostModal = (props) => {
+  const {
+    buttonLabel,
+    className
+  } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
+  return (
+    <div>
+      <Button color="dark" onClick={toggle}>{buttonLabel} Make a post!</Button>
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>Make a Post</ModalHeader>
+        <ModalBody>
+            <Form>
+                <FormGroup>
+                    <Label for="exampleText">Title</Label>
+                    <Input />
+                    <Label for="exampleText">Text Area</Label>
+                    <Input type="textarea" name="text" id="exampleText" />
+                </FormGroup>
+            </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>Post</Button>{' '}
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
+}
+
+export default PostModal;
